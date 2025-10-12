6f696e8f6601498363524fc58f3b0a6a912b5a53
Aman Kumar <aman9995199@gmail.com>
2025-10-12 19:12:17 +0530

    added data

--- netlify/functions/django.py
+++
@@ -1,9 +0,0 @@
-import os
-from django.core.wsgi import get_wsgi_application
-from serverless_wsgi import handle_request
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "budget_backend.settings")  # <-- change
-application = get_wsgi_application()
-
-def handler(event, context):
-    return handle_request(application, event, context)
